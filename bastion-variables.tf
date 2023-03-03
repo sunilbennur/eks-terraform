36cccb9cca7d02a68f5eb66a90c42468e3ada729
sunil rathod <sunilbennur25@gmail.com>
2023-03-03 21:00:12 +0530

    first commit

---
+++ bastion-variables.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type = string
+  default = "t3.micro"  
+}
+
+# AWS EC2 Instance Key Pair
+variable "instance_keypair" {
+  description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
+  type = string
+  default = "eks-terraform-key"
+}
+
+variable "ec2_name" {
+  default = "bastion-host"
+}
